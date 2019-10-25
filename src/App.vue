39e02d11c7485c2a0d9c0ab8a47ec34e36a67284
z17090257999 <893995194@qq.com>
2019-10-25 17:53:15 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    黑马头条pc项目
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="less">
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+// 入口文件
+// 1、创建根实例  #app  --->  pubulic下的index.html中的app部分
+// 2、依赖项目需要的全局资源
+
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
