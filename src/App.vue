3b9acb001328284dd8395489b45b3cc78ab820ef
z17090257999 <893995194@qq.com>
2019-10-25 19:37:15 +0800

    添加element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    黑马头条pc项目
+    黑马头条pc项目<el-button type="primary">主要按钮</el-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@
 // 2、依赖项目需要的全局资源
 
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
