d92eef578918864f25e2998f90f03abe4f3e5417
z17090257999 <893995194@qq.com>
2019-10-26 19:42:37 +0800

    创建首页路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,24 @@ import VueRouter from 'vue-router'
 import Vue from 'vue'
 
 import Login from '@/views/login'
+import Home from '@/views/home'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
-  routes: [{
-    path: '/login',
-    component: Login
-  }]
+  routes: [
+    // 登陆规则
+    {
+      path: '/login',
+      component: Login
+    },
+    // 首页规则
+    {
+      path: '/',
+      component: Home
+    }
+  ]
 })
 
 export default router
